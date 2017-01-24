ef37e0b0b9e65a01566aaccec49cd709f7c357cd
bencode <bencode@163.com>
2017-01-24 22:59:16 +0800

    feat: 优化ploverAssetsVendors配置
    直接配置在配置文件中，而不是在package.json中

--- test/vendor.js
+++ test/vendor.js
@@ -7,26 +7,42 @@ const plugin = require('../lib/plugin');
 
 
 describe('vendor', function() {
+  const root = pathUtil.join(__dirname, 'fixtures/vendor');
+
   it('should load assets modules', function() {
-    const root = pathUtil.join(__dirname, 'fixtures/vendor');
-    const app = plover({ applicationRoot: root });
+    const app = plover({
+      applicationRoot: root,
+      assets: {
+        vendors: [
+          'jquery',
+          'bootstrap',
+          { name: 'vue', dist: 'output' }
+        ]
+      }
+    });
     plugin(app);
 
-    const info = app.moduleResolver.resolve('jquery');
-    info.should.eql({
-      name: 'jquery',
-      version: '2.0.0',
-      path: pathUtil.join(root, 'node_modules/jquery'),
-      assets: true,
-      assetsRoot: '',
-      reload: false,
-      build: false
-    });
+    const resolver = app.moduleResolver;
+    resolver.resolve('jquery')
+      .should.eql({
+        name: 'jquery',
+        version: '2.0.0',
+        path: pathUtil.join(root, 'node_modules/jquery'),
+        assets: true,
+        assetsRoot: '',
+        reload: false,
+        build: false
+      });
+
+    resolver.resolve('bootstrap').path
+      .should.equal(pathUtil.join(root, 'node_modules/bootstrap/dist'));
+
+    resolver.resolve('vue').path
+      .should.equal(pathUtil.join(root, 'node_modules/vue/output'));
   });
 
 
-  it('should work without ploverAssetsVendor config', function() {
-    const root = pathUtil.join(__dirname, 'fixtures/empty');
+  it('coverage for no assets config', function() {
     const app = plover({ applicationRoot: root });
     plugin(app);
   });
